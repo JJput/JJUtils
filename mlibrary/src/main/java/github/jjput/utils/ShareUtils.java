9e6fa4c970039ebc7cb52411c05521eb7ae89669
JJ <281138580@qq.com>
2019-03-26 14:54:56 +0800

    更新ShareUtils

--- mlibrary/src/main/java/github/jjput/utils/ShareUtils.java
+++ mlibrary/src/main/java/github/jjput/utils/ShareUtils.java
@@ -2,6 +2,7 @@ package github.jjput.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 
 /**
  * @作者: JJ
@@ -12,64 +13,81 @@ import android.content.SharedPreferences;
  */
 public class ShareUtils {
 
-    private static final String NAME = "config";
+    private static String name = "config";
 
-    public static boolean putString(Context mContext, String key, String value) {
-        if (mContext == null)
-            return false;
-        else if (key == null)
-            return false;
-        else if (value == null)
-            return false;
-        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    public static void setName(String name) {
+        ShareUtils.name = name;
+    }
+
+    public static boolean putString(@NonNull Context mContext, @NonNull String key, @NonNull String value) {
+        return putString(name, mContext, key, value);
+    }
+
+    public static boolean putString(@NonNull String fileName, @NonNull Context mContext, @NonNull String key, @NonNull String value) {
+        SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sp.edit().putString(key, value).commit();
     }
 
-    public static String getString(Context mContext, String key, String defValue) {
-        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    public static String getString(@NonNull Context mContext, @NonNull String key, @NonNull String defValue) {
+        return getString(name, mContext, key, defValue);
+    }
+
+    public static String getString(@NonNull String fileName, @NonNull Context mContext, @NonNull String key, @NonNull String defValue) {
+        SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sp.getString(key, defValue);
     }
 
-    public static boolean putInt(Context mContext, String key, int value) {
-        if (mContext == null)
-            return false;
-        else if (key == null)
-            return false;
-        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    public static boolean putInt(@NonNull Context mContext, @NonNull String key, @NonNull int value) {
+        return putInt(name, mContext, key, value);
+    }
+
+    public static boolean putInt(@NonNull String fileName, @NonNull Context mContext, @NonNull String key, @NonNull int value) {
+        SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sp.edit().putInt(key, value).commit();
     }
 
-    public static int getInt(Context mContext, String key, int defValue) {
-        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    public static int getInt(@NonNull Context mContext, @NonNull String key, @NonNull int defValue) {
+        return getInt(name, mContext, key, defValue);
+    }
+
+    public static int getInt(@NonNull String fileName, @NonNull Context mContext, @NonNull String key, @NonNull int defValue) {
+        SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sp.getInt(key, defValue);
     }
 
-    public static boolean putBool(Context mContext, String key, Boolean value) {
-        if (mContext == null)
-            return false;
-        else if (key == null)
-            return false;
-        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    public static boolean putBool(@NonNull Context mContext, @NonNull String key, @NonNull Boolean value) {
+        return putBool(name, mContext, key, value);
+    }
+
+    public static boolean putBool(@NonNull String fileName, @NonNull Context mContext, @NonNull String key, @NonNull Boolean value) {
+        SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sp.edit().putBoolean(key, value).commit();
     }
 
-    public static Boolean getBool(Context mContext, String key, Boolean defValue) {
-        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    public static Boolean getBool(@NonNull Context mContext, @NonNull String key, @NonNull Boolean defValue) {
+        return getBool(name, mContext, key, defValue);
+    }
+
+    public static Boolean getBool(@NonNull String fileName, @NonNull Context mContext, @NonNull String key, @NonNull Boolean defValue) {
+        SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sp.getBoolean(key, defValue);
     }
 
-    public static boolean delSingle(Context mContext, String key) {
-        if (mContext == null)
-            return false;
-        else if (key == null)
-            return false;
+    public static boolean delSingle(@NonNull Context mContext, @NonNull String key) {
+        return delSingle(name, mContext, key);
+    }
 
-        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    public static boolean delSingle(@NonNull String fileName, @NonNull Context mContext, @NonNull String key) {
+        SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sp.edit().remove(key).commit();
     }
 
-    public static void delAll(Context mContext) {
-        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    public static void delAll(@NonNull Context mContext) {
+        delAll(name, mContext);
+    }
+
+    public static void delAll(@NonNull String fileName, @NonNull Context mContext) {
+        SharedPreferences sp = mContext.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         sp.edit().clear().commit();
     }
 
